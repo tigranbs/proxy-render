cabd8dce93d35d24b7d81a9e495bcafc396e95b7
Tigran Bayburtsyan <tigran@bayburtsyan.com>
2017-04-24 13:03:51 -0700

    adding rendering functionality

---
+++ configure.js
@@ -0,0 +1,85 @@
+/**
+ * Created by tigran on 4/24/17.
+ */
+
+/*
+ * Example for config
+ * module.exports = {
+ *   "view": "views/index",
+ *    "view_ending": "ejs",
+ *    "target": "http://localhost:3000/api",
+ *    "debug": true
+ * };
+ *
+ * module.exports = {
+ *   "view": {
+ *       "/login": "views/auth/login.ejs"
+ *   },
+ *    "view_ending": "ejs",
+ *    "target": "http://localhost:3000/api",
+ *    "debug": true
+ * };
+ *
+ * module.exports = {
+ *   "view": {
+ *       "/login": {
+ *           "name": "views/auth/login.ejs",
+ *           "target": "http://localhost:3000/api/login"
+ *       }
+ *   },
+ *    "view_ending": "ejs",
+ *    "target": "http://localhost:3000/api",
+ *    "debug": true
+ * };
+ *
+ * */
+
+const path = require("path")
+    , fs = require('fs')
+    , render_fn = require('./render');
+
+let render_config = {};
+
+module.exports = {
+    configure: function (app) {
+        let config_path = path.join(path.resolve("./"), 'proxy_render.config.js');
+
+        // Reading configuration file
+        try {
+            fs.accessSync(config_path, fs.constants.R_OK)
+        } catch (e) {
+            console.log(e);
+            console.log("Unable to find file 'proxy_render.config.js' for configuration options. Or maybe file is not accessible for current user.");
+            process.exit(1);
+        }
+
+        render_config = require(config_path);
+
+        // making debug as a boolean, if it's not a boolean
+        render_config.debug = !!render_config.debug;
+
+
+        switch (typeof render_config.view) {
+            case 'string':
+                // if only one view is defined, then using it for every request
+                app.use(render_fn(render_config.view));
+                break;
+            case 'object':
+                // Setting views for specific given routes
+                for(let key in render_config.view) {
+                    app.get(key, render_fn(render_config.view[key]), render_config.target);
+                }
+                break;
+            default:
+                console.log('"view" option is required to define views for specific request urls. Could be "string" or "object"');
+                process.exit(1);
+                break;
+        }
+
+        render_config.host = (typeof render_config.host === 'undefined' ? '0.0.0.0' : render_config.host);
+        render_config.port = (typeof render_config.port === 'undefined' ? '8000' : render_config.port);
+    },
+    getConfig: function () {
+        return render_config;
+    }
+};
--- render.js
+++ render.js
@@ -1,6 +1,64 @@
 /**
  * Created by tigran on 4/23/17.
  */
+
+const config = require('./configure').getConfig()
+    , ReactServerDOM = require('react-dom/server');
+
+// adding babel for requiring JSX files
+require('babel-register')({
+    extensions: ['.jsx', '.js'],
+    presets: ['es2015', 'react'],
+    // enabling babel-caching if we are in release mode
+    cache: !config.debug
+});
+
+let requireKeys = [];
+
+const clearRequireCache = () => {
+    let delete_keys = [];
+    for (let k in require.cache) {
+        if(requireKeys.indexOf(k) === -1) {
+            delete_keys.push(k);
+        }
+    }
+
+    while (delete_keys.length > 0) {
+        delete require.cache[delete_keys.pop()];
+    }
+};
+
+const fixRequireKeys = () => {
+    for(let k in require.cache) {
+        requireKeys.push(k);
+    }
+};
+
+const renderJSX = (path, props) => {
+    if(config.debug) {
+        fixRequireKeys();
+    }
+
+    let App = require(path).default;
+    let app = new App(props);
+    if(config.debug) {
+        clearRequireCache();
+    }
+
+    try {
+        return ReactServerDOM.renderToString(app);
+    } catch (e) {
+        console.log("Error while trying to render file: " + path);
+        console.log(e);
+    }
+
+    return false;
+};
+
+module.exports = {
+    jsx: renderJSX
+};
+
 module.exports = function (view_path, api_target) {
     let file_name = view_path;
     if(typeof view_path === 'object') {
