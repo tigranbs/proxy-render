7c7ed9089c2ebe7136533fb616210547c235268d
Tigran Bayburtsyan <tigran@bayburtsyan.com>
2017-04-24 13:32:44 -0700

    rendering based on proxy response

--- render.js
+++ render.js
@@ -4,7 +4,7 @@
 
 const config = require('./configure').getConfig()
     , ReactServerDOM = require('react-dom/server')
-    , request = require('request');
+    , targetProxy = require('./targetProxy');
 
 // adding babel for requiring JSX files
 require('babel-register')({
@@ -56,15 +56,20 @@ const renderJSX = (path, props) => {
     return false;
 };
 
-module.exports = function (view_path, api_target) {
+module.exports = function (view_path, render_app, api_target) {
     let file_name = view_path;
     if(typeof view_path === 'object') {
         file_name = view_path.name;
         api_target = (typeof view_path.target === 'undefined' ? api_target : view_path.target );
     }
 
-
     return function (req, res) {
-        res.render(file_name, {});
+        targetProxy(req, api_target)
+            .then(function (body) {
+                res.render(file_name, {html: renderJSX(render_app, body)});
+            })
+            .catch(function () {
+
+            });
     };
 };
